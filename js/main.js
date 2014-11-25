26b9e3a106d11cd7b609e38a3202a1a155243c19
Ellie Hoshizaki <ellie@mediahound.com>
2014-11-24 21:05:58 -0800

    added more content to contact section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 	console.log("hello");
+
+  $("#main-site-button").on('click', function(e){
+    e.preventDefault();
+    console.log("clicked button");
+    $('html, body').animate({
+      scrollTop: $("#main-content").offset().top
+    }, 300);
+  });
+
 });
